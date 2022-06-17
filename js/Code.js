91886f0c3b2cff7a2567b6a9c98c7515b9c6b907
ronavt <90161609+ronavt@users.noreply.github.com>
2022-06-16 19:42:09 -0500

    Add files via upload

---
+++ js/Code.js
@@ -0,0 +1,25 @@
+const accessCode1 = "VARIABLE";
+const accessCode2 = "FUNCTION";
+const accessCode3 = "DATABASE";
+
+function clues() {
+    
+    fill("white")
+    textSize(15)
+    text("B R V A I A L E", 100,50)
+    fill("lightblue")
+    text("Hint: Always changing, not constant !!", 100,70)
+
+    fill("white")
+    textSize(15)
+    text("O I N F U C T N", 700,150)
+    fill("lightblue")
+    text("Hint: Performs a particular task !!", 700,170)
+
+    fill("white")
+    textSize(15)
+    text("B A T A D A S E", 100,250)
+    fill("lightblue")
+    text("Hint: Stores all information !!", 100,270)
+
+}
